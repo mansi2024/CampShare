a6b3a5808434ba16a13e2c40fb72db4566c6fc94
Mansi_simu <89377143+mansi2024@users.noreply.github.com>
2024-03-04 11:04:56 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,12 +5,13 @@ const express = require('express');
 const app = express();
 const ejsMate = require('ejs-mate');
 const ExpressError = require('./Utils/ExpressError');
-const session = require('cookie-session');
+const session = require('express-session');
 const flash = require('connect-flash');
 const passport = require('passport');
 const LocalStrategy = require('passport-local');
 const User = require('./model/user');
 const mongoSanitize = require('express-mongo-sanitize');
+const MemoryStore = require('memorystore')(session);
 
 const userRoutes = require('./routes/users')
 const campgrounds = require('./routes/campgrounds');
@@ -46,7 +47,6 @@ app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname,'public')));
 
 
-
 const sessionConfig = {
   name:'session',
   secret : 'thisshouldbebettersecret',
@@ -57,7 +57,10 @@ const sessionConfig = {
     expires : Date.now() + 1000* 60*60*24*7,
     maxAge :1000* 60*60*24*7
 
-  }
+  },
+  store: new MemoryStore({
+    checkPeriod:86400000
+  }),
 }
 app.use(session(sessionConfig));
 app.use(flash());
